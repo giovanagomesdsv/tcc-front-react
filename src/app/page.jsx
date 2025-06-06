4bb1f1a8b303bfde33e4e091f039fcd647c793fe
Giovana Gomes <g.gomesdsv@gmail.com>
2025-06-06 11:11:46 -0300

    paginas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,6 +13,24 @@ export default function Home() {
       <Link href={"/livros"}>Livros</Link>
       <Link href={"/sobre"}>Sobre</Link>
 
+
+      <h1>Navegação</h1>
+
+      <p>ADMINISTRADOR</p>
+      <Link href={"/perfil"}>Perfil</Link>
+      <Link href={"/avaliar"}>Avaliar</Link>
+      <Link href={"/lista-livrarias"}>Livrarias</Link>
+      <Link href={"/lista-resenhistas"}>Resenhistas</Link>
+      <Link href={"/lista-livros"}>Livros</Link>
+      <Link href={"/usuarios"}>Usuários</Link>
+      <br />
+      <p>LIVRARIAS</p>
+      <Link href={"/anuncios"}>Anúncios</Link>
+      <br />
+      <p>RESENHISTAS E LIVRARIAS</p>
+      <Link href={"/perfil"}>Perfil</Link>
+      <Link href={"/criar-resenha"}>Criar Resenha</Link>
+      <Link href={"/minhas-resenhas"}>Minhas Resenhas</Link>
     </div>
   );
 }
---
+++ src/app/perfil/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Perfil() {
+  return (
+    <div className="container">
+      <h1>Perfil</h1>
+
+    </div>
+  );
+}
