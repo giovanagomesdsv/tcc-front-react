2e9d5772082407a504693d3f5e06a167ecaae6a0
MarianaArissa123 <arissamariana66@gmail.com>
2025-06-06 11:33:03 -0300

    teste

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -5,6 +5,73 @@ export default function Perfil() {
     <div className="container">
       <h1>Perfil</h1>
 
+      <header>
+        Administrador BC
+    </header>
+    <nav className="sidebar" id="sidebar">
+    <ul className="nav-list">
+        <div className="nome">
+            <li className="usuario">
+            <div className="fix"></div>
+            </li>
+            <div className="menu" id="menu">
+                <i className="bx bx-menu"></i>
+            </div>
+        </div>
+        <li>
+                <a href="../../index.php">
+                <i className='bx  bx-reply-stroke'></i> 
+                    <span className="link_name">Bibliófilos Community</span>
+                </a>
+            </li>
+            <li>
+                <a href="../home.php">
+                    <i className='bx bx-home-alt-2'></i>
+                    <span className="link_name">Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="../livrarias/livrarias.php">
+                    <i className='bx bx-user'></i>
+                    <span className="link_name">Livrarias</span>
+                </a>
+            </li>
+            <li>
+                <a href="../resenhistas/resenhistas.php">
+                    <i className='bx bx-user-pin'></i>
+                    <span className="link_name">Resenhistas</span>
+                </a>
+            </li>
+            <li>
+                <a href="../livro/livros.php">
+                    <i className='bx bx-book-bookmark'></i>
+                    <span className="link_name">Livros</span>
+                </a>
+            </li>
+            <li>
+                <a href="../usuarios/usuarios.php">
+                    <i className='bx bx-book-content'></i>
+                    <span className="link_name">Usuarios</span>
+                </a>
+            </li>
+            <li className="../sair">
+                <a href="logout.php"><i className='bx bx-log-out'></i></a>
+            </li>
+        </ul>
+    </nav>
+<main>
+    <div className="perfil-container">
+        <h1>Perfil do Usuário</h1>
+        <p><strong>Nome:</strong></p>
+        <p><strong>Email:</strong></p>
+        <p><strong>Senha:</strong></p>
+        <p><strong>Data de Criação:</strong></p>
+        <p><strong>Status:</strong> <span className="status"></span></p>
+        <a href="editar-perfil.php">Editar perfil</a>
+    </div>
+    </main>
+   
+
     </div>
   );
 }
