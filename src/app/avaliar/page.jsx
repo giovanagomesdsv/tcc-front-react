81987f1ca40bedbc54b94de81ffea75a3a008b79
CleversonM <cleversonmarceloacelino@gmail.com>
2025-06-09 08:55:37 -0300

    mudanças
    mudanças

--- src/app/avaliar/page.jsx
+++ src/app/avaliar/page.jsx
@@ -2,7 +2,10 @@ import styles from "./page.module.css";
 
 export default function Avaliar() {
     return (
-        <div className="container">
+        <div className={styles.Container}>
+        <header className={styles.HeaderBackground}>
+             BACKSTAGE Community
+        </header>
             <h1>Avaliar</h1>
         </div>
     )
