375a5c6783a895e96f2394002605016839136632
CleversonM <cleversonmarceloacelino@gmail.com>
2025-06-13 11:01:01 -0300

    mudanças
    mudanças

--- src/app/avaliar/avalicao-resenha/page.jsx
+++ src/app/avaliar/avalicao-resenha/page.jsx
@@ -1,9 +1,35 @@
 import styles from "./page.module.css";
 
-export default function AvaliacaoResenha() {
+export default function ReviewCard({ foto, titulo, sinopse, texto, autor, dado }) {
     return (
-        <div className="container">
-            <h1>AvaliacaoResenha</h1>
+      <>
+        <div className={`${styles.card} ${styles.card3}`}>
+          <div className={styles.cardimgsinopse}>
+            <div className={styles.sinopse}>
+              <p>LOREM IPSUM</p>
+              <p><strong>Sinopse</strong></p>
+              <p className={styles.resenhabox}>[ONDE VAI SE ENCAIXAR A RESENHA]</p>
+            </div>
+          </div>
+  
+          <div className={styles.cardresenha}>
+            <div className={styles.resenha}>
+              <p><strong>RESENHA</strong></p>
+              <p>{texto}</p>
+              <p>{autor}</p>
+            </div>
+          </div>
+  
+          <form className={styles.barraenvia} action={`?id=${dado}`} method="post">
+            <select className={styles.notas} name="avaliar" required>
+              <option className={styles.resultado} value="">Avaliar</option>
+              <option className={styles.resultado} value="1">Reprovada</option>
+              <option className={styles.resultado} value="3">Corrigir</option>
+              <option className={styles.resultado} value="2">Aprovada</option>
+            </select>
+            <input className={styles.teste} type="submit" value="Enviar" />
+          </form>
         </div>
-    )
-}
+      </>
+    );
+  }
--- src/app/avaliar/page.jsx
+++ src/app/avaliar/page.jsx
@@ -1,9 +1,76 @@
+import Link from "next/link";
 import styles from './page.module.css';
 
 export default function PaginaExemplo() {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+
+  <header>
+    Administrador BC
+  </header>
+  {/* NAVBAR */}
+  <nav className="sidebar" id="sidebar">
+          <ul className="navlist">
+            <div className="nome">
+              <li className="usuario">
+                <div className="fix"></div>
+              </li>
+              <div className="menu" id="menu">
+                <i className="bxbxmenu"></i>
+              </div>
+            </div>
+  
+            <li>
+              <a href="../../index.php">
+                <i className="bxbxreplystroke"></i>
+                <span className="linkname">Bibliófilos Community</span>
+              </a>
+            </li>
+  
+            <li>
+              <a href="../home.php">
+                <i className="bxbxhomealt2"></i>
+                <span className="linkname">Home</span>
+              </a>
+            </li>
+  
+            <li>
+              <a href="../livrarias/livrarias.php">
+                <i className="bxbxuser"></i>
+                <span className="linkname">Livrarias</span>
+              </a>
+            </li>
+  
+            <li>
+              <a href="../resenhistas/resenhistas.php">
+                <i className="bxbxuserpin"></i>
+                <span className="linkname">Resenhistas</span>
+              </a>
+            </li>
+  
+            <li>
+              <a href="../livro/livros.php">
+                <i className="bxbxbookbookmark"></i>
+                <span className="linkname">Livros</span>
+              </a>
+            </li>
+  
+            <li>
+              <a href="../usuarios/usuarios.php">
+                <i className="bxbxbookcontent"></i>
+                <span className="linkname">Usuarios</span>
+              </a>
+            </li>
+  
+            <li className="../sair">
+              <a href="logout.php">
+                <i className="bxbxlogout"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+  {/* INÍCIO DO SITE */}
+  <div className={styles.header}>
         <span className={styles.mensagem}>Olá Usuário, seja bem vindo!</span>
       </div>
       <div className={styles.avaliar}>
@@ -23,7 +90,9 @@ export default function PaginaExemplo() {
             <br />
             <p className={styles.inputText}>Usuario</p>
           </div>
+          <Link href={"/avaliar/avaliacao-livraria"}>
           <button className={styles.botao}>Avaliar</button>
+          </Link>
         </div>
       </div>
 
@@ -41,9 +110,9 @@ export default function PaginaExemplo() {
             <br />
             <p className={styles.inputText}>Usuario</p>
           </div>
-          <a href={"avaliar/avaliar-livraria"}>
+          <Link href={"/avaliar/avaliacao-livraria"}>
           <button className={styles.botao}>Avaliar</button>
-          </a>
+          </Link>
         </div>
       </div>
 
@@ -61,9 +130,9 @@ export default function PaginaExemplo() {
             <br />
             <p className={styles.inputText}>Usuario</p>
           </div>
-          <a href={"avaliar/avaliar-resenha"}>
+          <Link href={"/avaliar/avalicao-resenha"}>
           <button className={styles.botao}>Avaliar</button>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
