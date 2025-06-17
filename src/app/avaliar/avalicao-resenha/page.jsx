b6abaad666c0731686fd855647175a836c78240b
CleversonM <cleversonmarceloacelino@gmail.com>
2025-06-17 11:45:53 -0300

    mudanças
    mudanças

--- src/app/avaliar/avalicao-resenha/page.jsx
+++ src/app/avaliar/avalicao-resenha/page.jsx
@@ -15,7 +15,7 @@ export default function ReviewCard({ foto, titulo, sinopse, texto, autor, dado }
               <img
             src="/imagens/exemplo.jpg"
             alt="Exemplo"
-            className={styles.imagem}
+            className="imagem"
               />
               <p className={styles.resenhabox}>The standard Lorem Ipsum passage, used since the 1500s
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
