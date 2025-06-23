84ef80074e2feab32e47f0c4216231c790cde520
CleversonM <cleversonmarceloacelino@gmail.com>
2025-06-23 09:01:50 -0300

    mudanças
    mudanças

--- src/app/usuarios/page.jsx
+++ src/app/usuarios/page.jsx
@@ -1,77 +1,113 @@
-import styles from "./page.module.css";
 import Link from "next/link";
+import styles from "./page.module.css";
 
-export default function Usuarios() {
-    return (
-        <div className="container">
-            <header>
+export default function UsuariosPage() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
         BACKSTAGE Community
       </header>
-      <nav className="sidebar" id="sidebar">
-        <ul className="navlist">
 
+      <nav className={styles.sidebar} id="sidebar">
+        <ul className={styles.navlist}>
           <li>
-            <Link href="/perfil" className="linkname">
+            <Link href="/perfil" className={styles.linkname}>
               <i className="bx bx-home-alt-2"></i>
-              <span className="linkname">Perfil</span>
+              <span className={styles.linkname}>Perfil</span>
             </Link>
           </li>
-          {/* icone de menu
-            <div className="menu" id="menu">
-              <i className="bx bx-menu"></i>
-            </div>
-            */}
 
           <li>
-            <a href="/">
+            <Link href="/" className={styles.linkname}>
               <i className="bx bx-reply-stroke"></i>
-              <span className="linkname">Bibliófilos Community</span>
-            </a>
+              <span className={styles.linkname}>Bibliófilos Community</span>
+            </Link>
           </li>
 
           <li>
-            <Link href="/avaliar" className="linkname">
+            <Link href="/avaliar" className={styles.linkname}>
               <i className="bx bx-home-alt-2"></i>
-              <span className="linkname">Home</span>
+              <span className={styles.linkname}>Home</span>
             </Link>
           </li>
 
           <li>
-            <Link href="/lista-livrarias" className="linkname">
+            <Link href="/lista-livrarias" className={styles.linkname}>
               <i className="bx bx-user"></i>
-              <span className="linkname">Livrarias</span>
+              <span className={styles.linkname}>Livrarias</span>
             </Link>
           </li>
 
           <li>
-            <Link href="/lista-resenhistas" className="linkname">
+            <Link href="/lista-resenhistas" className={styles.linkname}>
               <i className="bx bx-user-pin"></i>
-              <span className="linkname">Resenhistas</span>
+              <span className={styles.linkname}>Resenhistas</span>
             </Link>
           </li>
 
           <li>
-            <Link href="/lista-livros" className="linkname">
+            <Link href="/lista-livros" className={styles.linkname}>
               <i className="bx bx-book-bookmark"></i>
-              <span className="linkname">Livros</span>
+              <span className={styles.linkname}>Livros</span>
             </Link>
           </li>
 
           <li>
-            <Link href="/usuarios" className="linkname fix">
+            <Link href="/usuarios" className={`${styles.linkname} ${styles.fix}`}>
               <i className="bx bx-book-content"></i>
-              <span className="linkname">Usuários</span>
+              <span className={styles.linkname}>Usuários</span>
             </Link>
           </li>
 
           <li>
-            <Link href="/login" className="linkname">
+            <Link href="/login" className={styles.linkname}>
               <i className="bx bx-log-out"></i>
-              <span className="linkname">Sair</span>
+              <span className={styles.linkname}>Sair</span>
             </Link>
           </li>
         </ul>
       </nav>
+
+      <main className={styles.main}>
+        {/* Barra de pesquisa */}
+        <div className={styles.buscaContainer}>
+          <div className={styles.botao}>
+            <Link href="/usuarios/cadastrar">Cadastrar usuário</Link>
+          </div>
+
+          <form className={styles.buscaForm}>
+            <input type="text" placeholder="nome do usuário" />
+            <button type="submit">
+              <i className="bx bx-search"></i>
+            </button>
+          </form>
         </div>
-    )
+
+        {/* Resultados de busca */}
+        <div className={styles.pesquisa}>
+          <div className={`${styles.card} ${styles.containerInput}`}>
+            <p className={styles.input}>Nome do Usuário Exemplo</p>
+            <div className={styles.containerBtnInput}>
+              <p className={styles.inputID}>RESENHISTA</p>
+              <Link href="/usuarios/editar/1">
+                <div className={styles.botao}>ATUALIZAR</div>
+              </Link>
+            </div>
+          </div>
+
+          <div className={`${styles.card} ${styles.containerInput} ${styles.inativo}`}>
+            <p className={styles.input}>Outro Usuário Inativo</p>
+            <div className={styles.containerBtnInput}>
+              <p className={styles.inputID}>LIVRARIA</p>
+              <Link href="/usuarios/editar/2">
+                <div className={styles.botao}>ATUALIZAR</div>
+              </Link>
+            </div>
+          </div>
+
+          {/* Você pode adicionar mais cards fixos como mockup visual */}
+        </div>
+      </main>
+    </div>
+  );
 }
