980a43e4db3ca72e3e8b8e93ceb2b2264447f4e8
CleversonM <cleversonmarceloacelino@gmail.com>
2025-06-24 13:44:47 -0300

    finalizado
    finalizado
    
    usuários
    cadastrar
    atualizar

--- src/app/usuarios/atualizar/page.jsx
+++ src/app/usuarios/atualizar/page.jsx
@@ -1,9 +1,25 @@
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Atualizar() {
-    return (
-        <div className="container">
-            <h1>Atualizar</h1>
-        </div>
-    )
+  return (
+    <div>
+      <form className={styles.format}>
+        <label className={styles.label}>
+          Status atual: <strong>ATIVO</strong>
+        </label>
+
+        <select name="status" required className={styles.select}>
+          <option value="">-- Selecione novo status --</option>
+          <option value="1">ATIVO</option>
+          <option value="2">DESATIVADO</option>
+        </select>
+
+        <input
+          type="submit"
+          value="Atualizar"
+          className={styles.inputEditar}
+        />
+      </form>
+    </div>
+  );
 }
--- src/app/usuarios/cadastrar/page.jsx
+++ src/app/usuarios/cadastrar/page.jsx
@@ -1,9 +1,51 @@
 import styles from "./page.module.css";
 
-export default function Cadastrar() {
-    return (
-        <div className="container">
-            <h1>Cadastrar</h1>
-        </div>
-    )
+export default function FormCadastrarUsuario() {
+  return (
+    <div>
+      <form className={styles.format2}>
+        <label className={styles.label}>E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          required
+          className={styles.inputEditar2}
+        />
+
+        <label className={styles.label}>Nome:</label>
+        <input
+          type="text"
+          name="nome"
+          required
+          className={styles.inputEditar2}
+        />
+
+        <label className={styles.label}>Senha:</label>
+        <input
+          type="password"
+          name="senha"
+          required
+          className={styles.inputEditar2}
+        />
+
+        <label className={styles.label}>Tipo de usuário:</label>
+        <select
+          name="usuario"
+          id="usuario"
+          required
+          className={styles.inputEditar2}
+        >
+          <option value="">Selecione...</option>
+          <option value="2">Administrador</option>
+          <option value="0">Resenhista</option>
+        </select>
+
+        <input
+          type="submit"
+          value="Cadastrar usuário"
+          className={styles.inputConfirmar}
+        />
+      </form>
+    </div>
+  );
 }
