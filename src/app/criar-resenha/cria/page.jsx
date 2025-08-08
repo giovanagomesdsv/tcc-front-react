4f52338e09209ef0357d9e6b77f36d4259fbaebd
CleversonM <cleversonmarceloacelino@gmail.com>
2025-08-08 11:46:34 -0300

    mudanças
    mudanças

--- src/app/criar-resenha/cria/page.jsx
+++ src/app/criar-resenha/cria/page.jsx
@@ -3,7 +3,21 @@ import styles from "./page.module.css";
 export default function Cria() {
     return (
         <div className="container">
-            <h1>Cria</h1>
+            <header><p style={{marginRight: '80%'}}> BIBLIÓFILOS Community </p></header>
+         <div className={styles.box1}>
+            {/*Tela1 */}
+            <div className={styles.boxdisplay}>
+                <img src='teste.png' alt='test.png' className={styles.imgbox}style={{border: '2px solid red'}}/>
+                <br/>
+                <p className={styles.color1} style={{marginLeft: '10rem'}}>Sinopse:</p>
+            </div>
+            <div className={styles.boxdisplay}>
+            <p className={styles.color1}>Resenha:</p>
+            </div>
+            <div className={styles.textbox}>
+              <p className={styles.textdisplay}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus repellendus ipsa amet sed blanditiis eum fuga ea ipsam dolores cupiditate molestiae, nesciunt cum quidem sint quos reiciendis! Sit, iusto veritatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, maiores dolorum ipsum necessitatibus, doloremque obcaecati optio hic nemo repudiandae esse totam ducimus accusantium! Nisi dolorem eos, voluptatum dicta magni ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quidem inventore obcaecati, officia facere perferendis explicabo delectus harum. Esse earum, quis assumenda minima architecto est aperiam reiciendis blanditiis explicabo sit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolorem amet ipsam provident ad ducimus voluptate quia pariatur harum, non id mollitia, laborum ea tenetur corporis! Tenetur eaque accusantium corporis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore deleniti perferendis iure fugit, voluptas voluptate eveniet commodi ea, voluptatem modi laudantium laborum! Dolor eos cumque, qui placeat culpa nesciunt cupiditate?</p>  
+            </div>
+         </div>
         </div>
     )
 }
