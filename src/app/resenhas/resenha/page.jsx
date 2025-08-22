6d3b1c617a2c8c3d73119bf84c1c6e78ac8e903c
CleversonM2007 <cleversonmarceloacelino@gmail.com>
2025-08-22 01:15:17 -0300

    construção da estrutura da página
    adicionei parte da estrutura e css da página.

--- src/app/resenhas/resenha/page.jsx
+++ src/app/resenhas/resenha/page.jsx
@@ -2,8 +2,14 @@ import styles from "./page.module.css";
 
 export default function Resenha() {
     return (
-        <div className="container">
-            <h1>Resenha</h1>
+    <div className="container">
+        <header className={styles.headerStyle}>text</header>
+            {/*Div à direita*/}
+        <div className={styles.sec1}>
+            <img src="/images/img1.jpg" alt="Imagem 1" />
+            <br/>
+            <img src="/images/img2.jpg" alt="Imagem 2" />
         </div>
+    </div>
     )
 }
