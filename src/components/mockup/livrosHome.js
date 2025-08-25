28ce7334328470d5cce4627836078ce6886c8401
MarianaArissa123 <arissamariana66@gmail.com>
2025-08-25 15:11:19 -0300

    teste

---
+++ src/components/mockup/livrosHome.js
@@ -0,0 +1,77 @@
+import azkaban from '../../../public/temp/imagens/livros/Harry Potter e o Prisioneiro de Azkaban.jpg';
+import camaraSecreta from '../../../public/temp/imagens/livros/Harry Potter e a Camara Secreta.jpg';
+import caliceFogo from '../../../public/temp/imagens/livros/Harry Potter e o Calice de Fogo.jpg';
+import fenix from '../../../public/temp/imagens/livros/Harry Potter e a Ordem da Fenix.jpg';
+import chamado from '../../../public/temp/imagens/livros/O chamado selvagem.jpg';
+import morte from '../../../public/temp/imagens/livros/Harry Potter e as Reliquias da Morte.jpg';
+import principe from '../../../public/temp/imagens/livros/Harry Potter e o Enigma do Principe.jpg';
+
+const livros = [
+    
+        {
+            "livro_titulo": "Harry Potter  e a Camara Secreta ",
+            "livro_sinopse": "Depois de férias aborrecidas na casa dos tios trouxas, está na hora de Harry Potter voltar a estudar. Coisas acontecem, no entanto, para dificultar o regresso de Harry. Persistente e astuto, o herói não se deixa intimidar pelos obstáculos e, com a ajuda dos fiéis amigos Weasley, começa o ano letivo na Escola de Magia e Bruxaria de Hogwarts. As novidades não são poucas. Novos colegas, novos professores, muitas e boas descobertas e um grande e perigosos desafio. Alguém ou alguma coisa ameaça a segurança e a tranquilidade dos membros de Hogwarts.",
+            "livro_editora": "Rocco",
+            "livro_isbn": "-8532529812",
+            "livro_ano": 1990,
+            "livro_classidd": "12+",
+            "livro_foto": camaraSecreta
+        },
+        {
+            "livro_titulo": "Harry Potter e o Prisioneiro de Azkaban",
+            "livro_sinopse": "As aulas estão de volta à Hogwarts e Harry Potter não vê a hora de embarcar no expresso a vapor que o levará de volta à escola de bruxaria. Mais uma vez suas férias na rua dos Alfeneiros foi triste e solitária. Com muita ação, humor e magia, Harry Potter e o prisioneiro de Azkaban traz de volta o gigante atrapalhado Rúbeo Hagrid, o sábio diretor Alvo Dumbledore, a exigente professora de transformação Minerva MacGonagall e o novo mestre Lupin, que guarda grandes surpresas para Harry.",
+            "livro_editora": "Rocco",
+            "livro_isbn": "-8532529828",
+            "livro_ano": 1990,
+            "livro_classidd": "12+",
+            "livro_foto":azkaban
+        },
+        {
+            "livro_titulo": "Harry Potter  e o Calice de Fogo",
+            "livro_sinopse": "Nesta aventura, o feiticeiro cresceu e está com 14 anos. O início do ano letivo de Harry Potter reserva muitas emoções, mágicas, e acontecimentos inesperados, além de um novo torneio em que os alunos de Hogwarts terão de demonstrar todas as habilidade mágicas e nãomágicas que vêm adquirindo ao longo de suas vidas. Harry é escolhido pelo Cálice de Fogo para competir como um dos campeões de Hogwarts, tendo ao lado seus fiéis amigos. Muitos desafios, feitiços, poções e confusões estão reservados para Harry. Além disso, ele terá que lidar ainda com os problemas comuns da adolescência amor, amizade, aceitação e rejeição.",
+            "livro_editora": "Rocco",
+            "livro_isbn": "-8532529835",
+            "livro_ano": 1990,
+            "livro_classidd": "12+",
+            "livro_foto":caliceFogo
+        },
+        {
+            "livro_titulo": "Harry Potter  e a Ordem da Fenix",
+            "livro_sinopse": "Nesta aventura, o feiticeiro cresceu e está com 14 anos. O início do ano letivo de Harry Potter reserva muitas emoções, mágicas, e acontecimentos inesperados, além de um novo torneio em que os alunos de Hogwarts terão de demonstrar todas as habilidade mágicas e nãomágicas que vêm adquirindo ao longo de suas vidas. Harry é escolhido pelo Cálice de Fogo para competir como um dos campeões de Hogwarts, tendo ao lado seus fiéis amigos. Muitos desafios, feitiços, poções e confusões estão reservados para Harry. Além disso, ele terá que lidar ainda com os problemas comuns da adolescência amor, amizade, aceitação e rejeição.",
+            "livro_editora": "Rocco",
+            "livro_isbn": "-8532529842",
+            "livro_ano": 1990,
+            "livro_classidd": "12+",
+            "livro_foto": fenix
+        },
+        {
+            "livro_titulo": "Harry Potter  e o Enigma do Principe",
+            "livro_sinopse": "Harry Potter e o enigma do príncipe dá continuidade à saga do jovem bruxo Harry Potter a partir do ponto em que o livro anterior parou o momento em que fica provado que o poder de Voldemort e dos Comensais da Morte, seus seguidores, cresce mais a cada dia, em meio à batalha entre o bem e o mal. A onda de terror provocada pelo Lorde das Trevas estaria afetando, até mesmo, o mundo dos trouxas (nãobruxos), e sendo agravada pela ação dos dementadores, criaturas mágicas aterrorizantes que sugam a esperança e a felicidade das pessoas. Harry, que acabou de completar 16 anos, parte rumo ao sexto ano na Escola de Magia e Bruxaria de Hogwarts, animado e ao mesmo tempo apreensivo com a perspectiva de ter aulas particulares com o professor Dumbledore, o diretor da escola e o bruxo mais respeitado em toda comunidade mágica. Harry, longe de ser aquele menino magricela que vivia no quarto debaixo da escada na casa dos tios trouxas.",
+            "livro_editora": "Rocco",
+            "livro_isbn": "-8532529859",
+            "livro_ano": 1990,
+            "livro_classidd": "12+",
+            "livro_foto":principe
+        },
+        {
+            "livro_titulo": "Harry Potter e as Relíquias da Morte",
+            "livro_sinopse": "Harry Potter  e as Relíquias da Morte, Harry Potter e as relíquias da morte, de J.K. Rowling, é o sétimo e último livro da série. Voldemorte está cada vez mais forte e Harry Potter precisa encontrar e aniquilar as Horcruxes para enfraquecer o lorde e poder enfrentálo. Nessa busca desenfreada, contando apenas com os amigos Rony e Hermione, Harry descobre as Relíquias da Morte, que serão úteis na batalha do bem contra o mal.",
+            "livro_editora": "Rocco",
+            "livro_isbn": "-8532529866",
+            "livro_ano": 1990,
+            "livro_classidd": "12+",
+            "livro_foto":morte
+        },
+        {
+            "livro_titulo": "O chamado selvagem",
+            "livro_sinopse": "Buck é um cachorro enorme e afável que leva uma boa vida em um rancho na Califórnia, comendo e dormindo ao sol. Porém, durante a febre do ouro nos anos 1890, ele é sequestrado e vendido para trabalhar puxando trenós no gélido norte da América.\nPassando por donos rudes e cruéis, Buck precisa lutar pela vida. À medida que aprende a se defender do porrete dos humanos e dos caninos de outros cães, ele cede a seus instintos primitivos, reconectando-se a seus antepassados.",
+            "livro_editora": "Arqueiro",
+            "livro_isbn": "-6555652173",
+            "livro_ano": 2002,
+            "livro_classidd": "12+",
+            "livro_foto":chamado
+        }
+];
+
+export default livros;
+
