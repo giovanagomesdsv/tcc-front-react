58f01d3e7b40264d9b1d96023692fc6b64a83467
CleversonM <cleversonmarceloacelino@gmail.com>
2025-08-26 12:20:31 -0300

    fazendo o autor
    fazendo a página autor, que é depois da página autores.

--- src/app/autores/autor/page.jsx
+++ src/app/autores/autor/page.jsx
@@ -3,7 +3,17 @@ import styles from "./page.module.css";
 export default function Autor() {
     return (
         <div className="container">
-            <h1>Autor</h1>
+
+            <header className={styles.headerStyle}></header>
+            
+            <div className={styles.sec1}>
+            <img src="/images/img4.jpg" alt="Imagem 3" className={styles.imagem3} />
+            </div>
+            
+            <div className={styles.sec2}>
+                <h1>Nome do Autor</h1>
+            </div>
+
         </div>
     )
 }
