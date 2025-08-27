ee623a09633289293881d3f760001bd9b5c79ca5
CleversonM <cleversonmarceloacelino@gmail.com>
2025-08-27 15:25:12 -0300

    página finalizada
    página autor finalizada

--- src/app/autores/autor/page.jsx
+++ src/app/autores/autor/page.jsx
@@ -18,6 +18,49 @@ export default function Autor() {
                 </p>
             </div>
             </div>
+
+<footer className="siteFooter">
+    {/* Logo redonda no canto esquerdo */}
+    <div className="footerLogo">
+        <img src="/logo.png" alt="Logo do site" className="footerLogo"/>
+    </div>
+
+    {/* Texto centralizado */}
+    <div>
+    <h3>Participe da nossa comunidade.</h3>
+    <br/>
+    <p className="footerTexto">Se torne um resenhista.<br/><br/>Entre em contato já!</p>
+    </div>
+
+    {/* Ícones das redes sociais no canto direito */}
+    <div className="footerRedes">
+            <a
+                href="https://twitter.com/seuperfil"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bx bxl-twitter"
+                aria-label="Twitter"
+            ></a>
+
+            <a
+                href="https://instagram.com/seuperfil"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bx bxl-instagram"
+                aria-label="Instagram"
+            ></a>
+
+            <a
+                href="https://tiktok.com/@seuperfil"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bx bxl-tiktok"
+                aria-label="TikTok"
+            ></a>
+        </div>
+</footer>
+
+
         </div>
     )
 }
