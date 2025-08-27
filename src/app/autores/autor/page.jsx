ecc4dbc2b92f0c216d606f019bb1191e721c768e
CleversonM <cleversonmarceloacelino@gmail.com>
2025-08-27 08:11:01 -0300

    mudanças
    mudanças

--- src/app/autores/autor/page.jsx
+++ src/app/autores/autor/page.jsx
@@ -5,15 +5,19 @@ export default function Autor() {
         <div className="container">
 
             <header className={styles.headerStyle}></header>
-            
+
+            <div className={styles.displayContainer}>
             <div className={styles.sec1}>
-            <img src="/images/img4.jpg" alt="Imagem 3" className={styles.imagem3} />
-            </div>
-            
-            <div className={styles.sec2}>
-                <h1>Nome do Autor</h1>
+                <img src="/images/img4.jpg" alt="Imagem 3" className={styles.imagem} />
             </div>
 
+            <div className={styles.sec2}>
+                <h1 className={styles.tituloAutor}>Titulo do Autor</h1>
+                <p className={styles.bio}>
+                Aluísio Azevedo (1857-1913) foi um escritor naturalista brasileiro. Suas obras mais conhecidas são "O Cortiço" e "Casa de Pensão", abordando a vida urbana e as desigualdades sociais do Brasil.
+                </p>
+            </div>
+            </div>
         </div>
     )
 }
