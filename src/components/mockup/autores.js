2119ecb4cc8f262c9649625feff7942045d0b04b
Giovana Gomes <g.gomesdsv@gmail.com>
2025-10-13 14:42:33 -0300

    mockup autores

---
+++ src/components/mockup/autores.js
@@ -0,0 +1,38 @@
+const autores = [
+    	{
+			"aut_id": 1,
+			"aut_nome": "Bernardo Guimarães",
+			"aut_bio": "Bernardo Guimarães (1825-1884) foi um escritor e poeta brasileiro, conhecido principalmente por suas obras de cunho nacionalista e regionalista, como o romance \"O Primo Basílio’.",
+			"aut_foto": "foto_bernardo_guimaraes.jpg"
+		},
+		{
+			"aut_id": 2,
+			"aut_nome": "Machado de Assis",
+			"aut_bio": "Machado de Assis (1839-1908) é considerado um dos maiores escritores da literatura brasileira. Com uma obra vasta, destacou-se como romancista, dramaturgo, poeta e crítico literário. Suas obras incluem \"Dom Casmurro\" e \"Memórias Póstumas de Brás Cubas\".",
+			"aut_foto": "foto_machado_assis.jpg"
+		},
+		{
+			"aut_id": 3,
+			"aut_nome": "Aluísio Azevedo",
+			"aut_bio": "Aluísio Azevedo (1857-1913) foi um escritor naturalista brasileiro. Suas obras mais conhecidas são \"O Cortiço\" e \"Casa de Pensão\", abordando a vida urbana e as desigualdades sociais do Brasil.",
+			"aut_foto": "foto_aluisio_azevendo.jpg"
+		},
+		{
+			"aut_id": 4,
+			"aut_nome": "Olavo Bilac",
+			"aut_bio": "Olavo Bilac (1865-1918) foi um poeta, jornalista e professor brasileiro, um dos principais nomes do Parnasianismo. Sua obra poética, com destaque para \"Poesias\", reflete o rigor formal e a beleza estética.",
+			"aut_foto": "foto_olavo_bilac.jpg"
+		},
+		{
+			"aut_id": 5,
+			"aut_nome": "Cruz e Sousa",
+			"aut_bio": "Cruz e Sousa (1861-1898) foi um poeta brasileiro, um dos maiores nomes do simbolismo no Brasil. Sua obra \"Broquéis\" é uma das mais importantes da literatura simbolista brasileira.",
+			"aut_foto": "foto_cruz_sousa.jpg"
+		},
+		{
+			"aut_id": 6,
+			"aut_nome": "Monteiro Lobato",
+			"aut_bio": "Monteiro Lobato (1882-1948) foi um escritor, editor e ativista brasileiro. Conhecido por suas obras infantis, especialmente a série do Sítio do Picapau Amarelo, ele também foi um importante defensor da educação e do desenvolvimento cultural no Brasil.",
+			"aut_foto": "foto_monteiro_lobato.jpg"
+		}
+]
