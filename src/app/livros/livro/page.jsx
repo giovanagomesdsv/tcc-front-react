7d848150f0dfa4b77a3925495cc6466f59852ab9
CleversonM <cleversonmarceloacelino@gmail.com>
2025-10-13 09:02:16 -0300

    fazendo a página
    fazendo a página livro.jsx

--- src/app/livros/livro/page.jsx
+++ src/app/livros/livro/page.jsx
@@ -2,9 +2,21 @@ import styles from "./page.module.css";
 
 export default function Livro() {
     return (
-        <div className="container">
-            <h1>Livro</h1>
+    <div className="container">
+            
+        <div className={styles.containerLivro}>
+                
+            <div className={styles.espacoEsq}>
+                <img src="/livro.png" className={styles.livroFoto} />
+            </div>
+            <div className={styles.espacoDir}>
+                <h1>Livro</h1>
+            </div>
+
+
 
         </div>
+
+    </div>
     )
 }
