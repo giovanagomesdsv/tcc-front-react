ddac4f6b6c6e315cc485d8a5ba449b94d5bfc7c9
Giovana Gomes <g.gomesdsv@gmail.com>
2025-10-14 08:18:03 -0300

    mockup autores

---
+++ src/components/autoresBC/index.js
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function autoresBC() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- src/components/mockup/autores.js
+++ src/components/mockup/autores.js
@@ -1,38 +1,45 @@
+import bernardo from '../../public/imagens/foto_bernardo_guimaraes.jpg';
+import machado from '../../public/imagens/foto_machado_assis.jpg';
+import aluisio from '../../public/imagens/foto_aluisio_azevendo.jpg';
+import olavo from '../../public/imagens/foto_olavo_bilac.jpg';
+import cruz from '../../public/imagens/foto_cruz_sousa.jpg';
+import monteiro from '../../public/imagens/foto_monteiro_lobato.jpg';
+
 const autores = [
     	{
 			"aut_id": 1,
 			"aut_nome": "Bernardo Guimarães",
 			"aut_bio": "Bernardo Guimarães (1825-1884) foi um escritor e poeta brasileiro, conhecido principalmente por suas obras de cunho nacionalista e regionalista, como o romance \"O Primo Basílio’.",
-			"aut_foto": "foto_bernardo_guimaraes.jpg"
+			"aut_foto": bernardo
 		},
 		{
 			"aut_id": 2,
 			"aut_nome": "Machado de Assis",
 			"aut_bio": "Machado de Assis (1839-1908) é considerado um dos maiores escritores da literatura brasileira. Com uma obra vasta, destacou-se como romancista, dramaturgo, poeta e crítico literário. Suas obras incluem \"Dom Casmurro\" e \"Memórias Póstumas de Brás Cubas\".",
-			"aut_foto": "foto_machado_assis.jpg"
+			"aut_foto": machado
 		},
 		{
 			"aut_id": 3,
 			"aut_nome": "Aluísio Azevedo",
 			"aut_bio": "Aluísio Azevedo (1857-1913) foi um escritor naturalista brasileiro. Suas obras mais conhecidas são \"O Cortiço\" e \"Casa de Pensão\", abordando a vida urbana e as desigualdades sociais do Brasil.",
-			"aut_foto": "foto_aluisio_azevendo.jpg"
+			"aut_foto": aluisio
 		},
 		{
 			"aut_id": 4,
 			"aut_nome": "Olavo Bilac",
 			"aut_bio": "Olavo Bilac (1865-1918) foi um poeta, jornalista e professor brasileiro, um dos principais nomes do Parnasianismo. Sua obra poética, com destaque para \"Poesias\", reflete o rigor formal e a beleza estética.",
-			"aut_foto": "foto_olavo_bilac.jpg"
+			"aut_foto": olavo
 		},
 		{
 			"aut_id": 5,
 			"aut_nome": "Cruz e Sousa",
 			"aut_bio": "Cruz e Sousa (1861-1898) foi um poeta brasileiro, um dos maiores nomes do simbolismo no Brasil. Sua obra \"Broquéis\" é uma das mais importantes da literatura simbolista brasileira.",
-			"aut_foto": "foto_cruz_sousa.jpg"
+			"aut_foto": cruz
 		},
 		{
 			"aut_id": 6,
 			"aut_nome": "Monteiro Lobato",
 			"aut_bio": "Monteiro Lobato (1882-1948) foi um escritor, editor e ativista brasileiro. Conhecido por suas obras infantis, especialmente a série do Sítio do Picapau Amarelo, ele também foi um importante defensor da educação e do desenvolvimento cultural no Brasil.",
-			"aut_foto": "foto_monteiro_lobato.jpg"
+			"aut_foto": monteiro
 		}
 ]
