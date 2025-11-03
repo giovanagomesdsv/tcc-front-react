421e47250844e7012a4780a84b077bfb65894fef
CleversonM <cleversonmarceloacelino@gmail.com>
2025-11-03 15:19:34 -0300

    página m-resenhas
    quase finalizada falta só colocar 2 por row

--- src/app/minhas-resenhas/page.jsx
+++ src/app/minhas-resenhas/page.jsx
@@ -3,8 +3,46 @@ import styles from "./page.module.css";
 export default function MResenhas() {
   return (
     <div className="container">
-      <h1>Minhas Resenhas</h1>
+
+      <header style={{ justifyContent: 'left'}}>
+        <p style={{ marginLeft: "10rem" }}>BACKSTAGE Community</p>
+      </header>
+
+          <div className={styles.containerResenhaBusca}>
+          <div className={styles.contador}>
+            <p className={styles.contadorBox}>
+              Resenhas publicadas: <span className={styles.contadorNumero}>0</span>
+            </p>
+          </div>
+
+          <div className="buscacontainer">
+            <form action="" method="GET" className="buscaform">
+              <input type="text" name="busca" placeholder="nome do resenhista" />
+              <button type="submit">
+                <i className="bx bx-search"></i>
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className={styles.resenhaBox}>
+        <div className={styles.resenhaImgEBox}>
+          <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
+
+          <div className={styles.resenhaTextoBox}>
+            <p className={styles.tituloResenha}>Título do livro</p>
+            <p className={styles.sinopseResenha}>Sinopse do livro</p>
+          </div>
+        </div>
+
+        <div className={styles.resenhaBotoes}>
+          <button className={styles.btn}><p className={styles.btnTexto}>ABRIR</p></button>
+          <button className={styles.btn}><p className={styles.btnTexto}>STATUS:</p></button>
+          <button className={styles.btn}><p className={styles.btnTexto}>ATUALIZAR</p></button>
+        </div>
+      </div>
 
     </div>
+
   );
 }
