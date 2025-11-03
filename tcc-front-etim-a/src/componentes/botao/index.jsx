c6ed6a588c0007c4dd60873b5e4e56d525faaded
Giovana Gomes <g.gomesdsv@gmail.com>
2025-11-03 08:46:07 -0300

    mochup resenha

--- tcc-front-etim-a/src/componentes/botao/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.module.css';
-
-export default function Botao() {
-    return(
-        <button className={styles.botao}>
-            Clique aqui
-        </button>
-    )
-}
