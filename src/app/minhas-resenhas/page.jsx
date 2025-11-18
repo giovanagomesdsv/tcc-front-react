ae1f659680047a749c6dd44b39c806a68264cb4e
CleversonM <cleversonmarceloacelino@gmail.com>
2025-11-18 10:19:42 -0300

    mudanças
    mudanças

--- src/app/minhas-resenhas/page.jsx
+++ src/app/minhas-resenhas/page.jsx
@@ -4,61 +4,98 @@ export default function MResenhas() {
   return (
     <div className="container">
 
-      <header style={{ justifyContent: 'left'}}>
+      <header style={{ justifyContent: 'left' }}>
         <p style={{ marginLeft: "10rem" }}>BACKSTAGE Community</p>
       </header>
 
-          <div className={styles.containerResenhaBusca}>
-          <div className={styles.contador}>
-            <p className={styles.contadorBox}>
-              Resenhas publicadas: <span className={styles.contadorNumero}>0</span>
-            </p>
-          </div>
-
-          <div className="buscacontainer">
-            <form action="" method="GET" className="buscaform">
-              <input type="text" name="busca" placeholder="nome do resenhista" />
-              <button type="submit">
-                <i className="bx bx-search"></i>
-              </button>
-            </form>
-          </div>
+      <div className={styles.containerResenhaBusca}>
+        <div className={styles.contador}>
+          <p className={styles.contadorBox}>
+            Resenhas publicadas: <span className={styles.contadorNumero}>0</span>
+          </p>
         </div>
-    <div className={styles.resenhasContainer}>
+
+        <div className="buscacontainer">
+          <form action="" method="GET" className="buscaform">
+            <input type="text" name="busca" placeholder="nome do resenhista" />
+            <button type="submit">
+              <i className="bx bx-search"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className={styles.resenhasContainer}>
         <div className={styles.resenhaBox}>
-        <div className={styles.resenhaImgEBox}>
-          <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
+          <div className={styles.resenhaImgEBox}>
+            <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
 
-          <div className={styles.resenhaTextoBox}>
-            <p className={styles.tituloResenha}>Título do livro</p>
-            <p className={styles.sinopseResenha}>Sinopse do livro</p>
+            <div className={styles.resenhaTextoBox}>
+              <p className={styles.tituloResenha}>Título do livro</p>
+              <p className={styles.sinopseResenha}>Sinopse do livro</p>
+            </div>
+          </div>
+
+          <div className={styles.resenhaBotoes}>
+            <button className={styles.btn}><a href="/minhas-resenhas/abrir" className={styles.btnLink}><p className={styles.btnTexto}>ABRIR</p></a></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>STATUS: APROVADO</p></button>
+            <button className={styles.btn}><a href="/minhas-resenhas/atualizar" className={styles.btnLink}><p className={styles.btnTexto}>ATUALIZAR</p></a></button>
           </div>
         </div>
 
-        <div className={styles.resenhaBotoes}>
-          <button className={styles.btn}><a href="/minhas-resenhas/abrir" className={styles.btnLink}><p className={styles.btnTexto}>ABRIR</p></a></button>
-          <button className={styles.btn}><p className={styles.btnTexto}>STATUS:</p></button>
-          <button className={styles.btn}><a href="/minhas-resenhas/atualizar" className={styles.btnLink}><p className={styles.btnTexto}>ATUALIZAR</p></a></button>
-        </div>
-      </div>
+        <div className={styles.resenhaBox}>
+          <div className={styles.resenhaImgEBox}>
+            <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
 
-      <div className={styles.resenhaBox}>
-        <div className={styles.resenhaImgEBox}>
-          <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
+            <div className={styles.resenhaTextoBox}>
+              <p className={styles.tituloResenha}>Título do livro</p>
+              <p className={styles.sinopseResenha}>Sinopse do livro</p>
+            </div>
+          </div>
 
-          <div className={styles.resenhaTextoBox}>
-            <p className={styles.tituloResenha}>Título do livro</p>
-            <p className={styles.sinopseResenha}>Sinopse do livro</p>
+
+          <div className={styles.resenhaBotoes}>
+            <button className={styles.btn}><p className={styles.btnTexto}>ABRIR</p></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>STATUS: APROVADO</p></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>ATUALIZAR</p></button>
           </div>
         </div>
 
-        <div className={styles.resenhaBotoes}>
-          <button className={styles.btn}><p className={styles.btnTexto}>ABRIR</p></button>
-          <button className={styles.btn}><p className={styles.btnTexto}>STATUS:</p></button>
-          <button className={styles.btn}><p className={styles.btnTexto}>ATUALIZAR</p></button>
+        <div className={styles.resenhaBox}>
+          <div className={styles.resenhaImgEBox}>
+            <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
+
+            <div className={styles.resenhaTextoBox}>
+              <p className={styles.tituloResenha}>Título do livro</p>
+              <p className={styles.sinopseResenha}>Sinopse do livro</p>
+            </div>
+          </div>
+
+
+          <div className={styles.resenhaBotoes}>
+            <button className={styles.btn}><p className={styles.btnTexto}>ABRIR</p></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>STATUS: REPROVADO</p></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>ATUALIZAR</p></button>
+          </div>
+        </div>
+
+        <div className={styles.resenhaBox}>
+          <div className={styles.resenhaImgEBox}>
+            <img src="resenhafoto.png" className={styles.resenhaImg} alt="Capa do livro" />
+
+            <div className={styles.resenhaTextoBox}>
+              <p className={styles.tituloResenha}>Título do livro</p>
+              <p className={styles.sinopseResenha}>Sinopse do livro</p>
+            </div>
+          </div>
+
+
+          <div className={styles.resenhaBotoes}>
+            <button className={styles.btn}><p className={styles.btnTexto}>ABRIR</p></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>STATUS: CORREÇÃO</p></button>
+            <button className={styles.btn}><p className={styles.btnTexto}>ATUALIZAR</p></button>
+          </div>
         </div>
       </div>
-    </div>
 
     </div>
 
